aaa57017346bc1ce1650f06ccbb7a76b427c09c3
m.sotnikov <m.sotnikov@fzlabs.ru>
2023-06-10 22:10:06 +0700

    db fix

--- modules/schemamanager/src/main/resources/db/migrations/V1/V1.1__create_tables_configuration_action_bot.sql
+++ modules/schemamanager/src/main/resources/db/migrations/V1/V1.1__create_tables_configuration_action_bot.sql
@@ -7,10 +7,18 @@ create table if not exists configuration
 
 create table if not exists action
 (
+    "name"      varchar(50)  primary key,
+    description varchar(100) not null
+    );
+
+create table if not exists action_field
+(
     id          serial primary key,
     "name"      varchar(50)  not null,
     description varchar(100) not null,
-    data        json         not null
+    "type"      varchar(100) not null,
+    action_name   varchar(50)  not null,
+    constraint fk_action foreign key (action_id) references action ("name")
 );
 
 create table if not exists bot
--- modules/schemamanager/src/main/resources/db/migrations/V1/V1.2__insert_default_test_values.sql
+++ modules/schemamanager/src/main/resources/db/migrations/V1/V1.2__insert_default_test_values.sql
@@ -1,11 +1,20 @@
-INSERT INTO bot.action ("name", description, data)
-VALUES ('Start', 'Начало работы с ботом',
-        format('{"id": 1, "name": "start", "next_node_id": 3, "start": {}}')::json);
-
 INSERT INTO bot.configuration("name", data)
-VALUES ('Test configuration', format(
-        '{"testConfig": {}}'
-    )::json);
+VALUES ('Test configuration', format('{"testConfig": {}}')::json);
 
 INSERT INTO bot.bot ("name", configuration_id)
 VALUES ('testBot', 1);
+
+INSERT INTO bot.action ("name", description)
+VALUES ('Start', 'Начало работы с ботом');
+
+INSERT INTO bot.action ("name", description)
+VALUES ('Button', 'Кнопка в меню бота');
+
+INSERT INTO bot.action ("name", description)
+VALUES ('Message', 'Отправка сообщения');
+
+INSERT INTO bot.action_field ("name", description, "type", action_name)
+VALUES ('Title', 'Заголовок кнопки', 'STRING', 'Button');
+
+INSERT INTO bot.action_field ("name", description, "type", action_name)
+VALUES ('Message', 'Ответ бота', 'STRING', 'Message');
