f58e59b437c97b2fedf2088491baf42a26efeb4b
i.bronnikov <ember.traveller@gmail.com>
2023-07-09 16:10:24 +0700

    Remove redudant script files for docker

--- modules/pulling_telegram_proxy/build_image.sh
+++
@@ -1,1 +0,0 @@
-docker build -f ./Dockerfile -t ghcr.io/for-run-coders/pulling_telegram_proxy ../
--- modules/pulling_telegram_proxy/push_image.sh
+++
@@ -1,3 +0,0 @@
-./build_image.sh
-docker login ghcr.io
-docker push ghcr.io/for-run-coders/pulling_telegram_proxy
